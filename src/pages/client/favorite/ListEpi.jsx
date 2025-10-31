0a422be0cb03775d87e89ac2c95d932b6a4fcd6f
thanhti <tranthanhtiqn@gmail.com>
2025-10-31 10:11:53 +0700

    titt

--- src/pages/client/favorite/ListEpi.jsx
+++ src/pages/client/favorite/ListEpi.jsx
@@ -8,7 +8,8 @@ import { ContextAuth } from "../../../contexts/AuthProvider";
 import { ContextPlayListMovies } from "../../../contexts/PlayListMoviesProvider";
 import { filterById, getOjectById } from "../../../services/reponsitory";
 import { ContextMovies } from "../../../contexts/MovieProvider";
-import { GiCancel } from "react-icons/gi";
+import { CiMenuKebab } from "react-icons/ci";
+import { RiArrowGoBackLine } from "react-icons/ri";
 
 function ListEpi() {
     const playLists = useContext(ContextPlayLists);
@@ -16,6 +17,7 @@ function ListEpi() {
     const [open, setOpen] = useState(false);
     const [editItem, setEditItem] = useState(null);
     const [selectedList, setSelectedList] = useState(null);
+    const [openMenu, setOpenMenu] = useState(null);
     const movies = useContext(ContextMovies);
 
     const handleClose = () => {
@@ -37,45 +39,65 @@ function ListEpi() {
             await deleteDocument("PlayLists", id);
         }
     };
-    const selectMovies = selectedList && filterById(playListMovies, selectedList.id, "idPlayList");
     const handleDeleteMovieFromList = async (id) => {
         if (window.confirm("Bạn có chắc muốn xóa phim này khỏi danh sách không?")) {
             await deleteDocument("PlayListMovies", id);
         }
     };
+
+    const handleShowMenu = (id) => {
+        setOpenMenu(openMenu == id ? null : id)
+    }
+    const handleShowList = (id) => {
+        setSelectedList(id);
+    }
+
     return (
         <div className="w-full text-white">
             <div>
-                <div className="flex items-center gap-3">
-                    <h1 className="text-xl">Danh sách</h1>
-                    <button
-                        onClick={() => setOpen(true)}
-                        className="border py-1 px-2 rounded-full text-xs flex items-center gap-2 hover:scale-95 active:scale-95"
-                    >
-                        <FaPlus /> Thêm mới
-                    </button>
+                <div className="flex justify-between gap-3">
+                    <div className="flex items-center gap-3">
+                        <h1 className="text-xl">Danh sách</h1>
+                        <button
+                            onClick={() => setOpen(true)}
+                            className="border py-1 px-2 rounded-full text-xs flex items-center gap-2 hover:scale-95 active:scale-95"
+                        >
+                            <FaPlus /> Thêm mới
+                        </button>
+                    </div>
+                    {
+                        selectedList && <button
+                            onClick={() => setSelectedList(null)}
+                            className="py-1 px-2 text-md flex items-center gap-2 hover:scale-95 active:scale-95"
+                        >
+                            <RiArrowGoBackLine /> quay lại
+                        </button>
+                    }
                 </div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+                <div className={`flex items-center gap-4 mt-5 ${selectedList ? "hidden" : ""}`}>
                     {playLists?.length > 0 ? (
                         playLists.map((p) => (
                             <div
-                                onClick={() => setSelectedList(selectedList?.id === p.id ? null : p)}
+                                onClick={() => handleShowList(p.id)}
                                 className={`bg-[#2e3355] rounded-lg p-4 w-[200px] flex flex-col gap-1
-                                             hover:bg-[#3b4066] transition-all ${selectedList?.id == p.id ? "bg-blue-700/50" : ""}`}
+                                             hover:bg-[#3b4066] transition-all ${selectedList?.id == p.id ? "bg-blue-700/50 " : ""}`}
                             >
-                                <span className="text-base">{p.name}</span>
+
                                 <div className="flex items-center justify-between">
-                                    <p>{filterById(playListMovies, p.id, "idPlayList").length} phim</p>
-                                    <div className="flex gap-3 text-lg">
-                                        <button onClick={() => handleEdit(p)}>
-                                            <FaEdit className="text-yellow-400 hover:scale-110" />
-                                        </button>
-                                        <button onClick={() => handleDelete(p.id)}>
-                                            <FaTrash className="text-red-500 hover:scale-110" />
-                                        </button>
+                                    <span className="text-base">{p.name}</span>
+
+                                    <div className="flex gap-3 text-lg relative">
+                                        <CiMenuKebab onClick={(e) => { e.stopPropagation(); handleShowMenu(p.id) }} className="" />
+                                        {openMenu === p.id && (
+                                            <div className="absolute bg-gray-600 top-5 z-10 p-2 rounded">
+                                                <p onClick={() => handleEdit(p)} className="text-yellow-500 flex items-center gap-2">Edit <FaEdit /></p>
+                                                <p onClick={() => handleDelete(p.id)} className="text-red-500 flex items-center gap-2">Delete <FaTrash /></p>
+                                            </div>
+                                        )}
                                     </div>
                                 </div>
+                                <p>{filterById(playListMovies, p.id, "idPlayList").length} phim</p>
                             </div>))
                     ) : (
                         <p className="text-gray-400 italic">Chưa có danh sách nào</p>
@@ -83,20 +105,20 @@ function ListEpi() {
                 </div>
                 {selectedList && (
                     <div className="flex items-center gap-5">
-                        {selectMovies.map(e => (
-                            <div className="bg-gray-700 rounded-md w-[200px] h-[320px] mt-5 hover:scale-102">
+                        {filterById(playListMovies, selectedList, "idPlayList").map(e => (
+                            <div className="bg-gray-700 rounded-md w-[200px] h-[310px] mt-5 hover:scale-102">
                                 <div className="relative ">
                                     <img src={getOjectById(movies, e.idMovie)?.poster || getOjectById(movies, e.idMovie)?.imgUrl}
                                         alt=""
                                         className="rounded-lg  object-cover w-full h-[250px]"
                                     />
-                                    <GiCancel
+                                    <FaTrash
                                         onClick={() => handleDeleteMovieFromList(e.id)}
-                                        className="text-red-500 text-2xl absolute top-0 right-0 translate-x-1/2 -translate-y-1/2"
+                                        className="text-red-500 text-md absolute top-0 right-0 p-1 text-2xl"
                                     />
                                     <p className="text-sm absolute bottom-0 bg-green-500 rounded-tr-xl p-1">{getOjectById(movies, e.idMovie)?.movieType}</p>
                                 </div>
-                                <p className="text-sm mt-2 p-1">{getOjectById(movies, e.idMovie)?.name}</p>
+                                <p className="text-sm mt-2 px-1 text-center">{getOjectById(movies, e.idMovie)?.name}</p>
                             </div>
                         ))
                         }
--- src/pages/client/favorite/ManageAccount.jsx
+++ src/pages/client/favorite/ManageAccount.jsx
@@ -6,7 +6,7 @@ function ManageAccount(props) {
     return (
         <div className='w-full flex mt-20 p-10 gap-10 items-start'>
             <Favorite/>
-            <div className='mb-auto'>
+            <div className='mb-auto flex-1'>
                 <Outlet/>
             </div>
         </div>
